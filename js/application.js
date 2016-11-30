70996b3eb8c10f0a977d30d3a6dafd86007cc1ec
Raulvv <raulvprivate@gmail.com>
2016-11-30 15:58:31 +0100

    Adding listener to game options

--- js/application.js
+++ js/application.js
@@ -5,4 +5,12 @@ var options = {
 var game = new TenSecondsMathGame(options);
 
 $(document).ready(function(){
+  $("#number-limit-range").on('change', function(){
+    options.numberLimit = this.value;
+    $("#number-limit").html(this.value);
+  });
+
+  $(".option-box").on('change', function(){
+    options.operators.push(this.id);
+  });
 });
