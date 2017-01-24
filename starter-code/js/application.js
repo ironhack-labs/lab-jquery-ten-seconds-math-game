0843b92ab5e0e0daf1991c6ecf4e5023d68284a5
Jeff <jzinger100@gmail.com>
2017-01-24 09:39:00 +0100

    game working with user operations

--- starter-code/js/application.js
+++ starter-code/js/application.js
@@ -3,6 +3,48 @@
 //Initialize ion library
 
 
-window.onload = function(){
+// window.onload = function(){
+//
+// };
 
+var userOperator, game;
+
+function getUserOperator(){
+  var numberOfCheckboxes = document.getElementsByClassName("userOperator").length;
+  var checkboxes = document.getElementsByClassName("userOperator");
+
+  //Assign operator according to button checked - when start button is clicked.
+
+  for (var i=0; i < numberOfCheckboxes; i++){
+    if(checkboxes[i].checked == true) {
+      switch(checkboxes[i].id){
+        case "addCheck":
+          userOperator = "+";
+          break;
+        case "minusCheck":
+          userOperator = "-";
+          break;
+        case "multCheck":
+          userOperator = "*";
+          break;
+        case "divideCheck":
+          userOperator = "/";
+          break;
+      }
+    }
+  }
+
+  //Assign limit based on what user has selected on sliding scale
+
+//Generate new game based on user operator and limit selected
+
+game = new TenSecondsMathGame(userOperator,10);
+//Function to start first game, HTML not shown until start game button is clicked
+game.newQuestion();
+}
+
+document.getElementById("start").onclick = function(){
+  getUserOperator();
+  document.getElementById("game-started").setAttribute("class", "display");
+  document.getElementById("start-screen").setAttribute("class", "display-none");
 };
--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,13 +1,114 @@
 // Use this file to write the logic of your game, the needed attrs and functions
 
-var TenSecondsMathGame = function(options) {
+var userOperation, userTopLimit;
 
-};
+// New game constructor
 
-// Returns a random integer between [1..numberLimit]
+function TenSecondsMathGame(operation, topLimit) {
+    this.userOperation = operation;
+    this.userTopLimit = topLimit;
+    this.newQuestion = questionGen;
+    this.isCorrectAnswer = isCorrectAnswer;
+}
+
+var randomGen, answer;
+
+function questionGen() {
+
+  // Returns a random integer between [1..numberLimit]
+
+  randomGen = function() {
+    return Math.floor(Math.random() * (game.userTopLimit - 1)) +1;
+  };
+
+  // Returns an object with {question, answer}
+
+  var random1 = randomGen();
+  var random2 = randomGen();
+
+//Reruns random number generators if difference is between 0 and 3
+
+  var reRunRandom = function(){
+    while((random1 - random2 < 3 &&  random1 - random2 >= 0) || (random2 - random1 < 3 && random2 - random1 >= 0)){
+    random1 = randomGen();
+    random2 = randomGen();
+    }
+  };
+
+  //Generates correct answer to question generated
+
+  switch(this.userOperation) {
+    case '+':
+      reRunRandom();
+      answer = random1 + random2;
+      break;
+    case '-':
+      reRunRandom();
+      if(random1 < random2){
+          answer = random2 - random1;
+      } else {
+        answer = random1 - random2;
+      }
+      break;
+    case '*':
+      reRunRandom();
+      answer = random1 * random2;
+      break;
+    case '/':
+      answer = random1 / random2;
+      break;
+
+  }
+
+//HTML section
+
+//Shows which operator has been selected in HTML
+
+document.getElementById("operation").innerHTML = this.userOperation;
+
+//Switched HTML numbers depending on which is bigger
+
+  if (random1 > random2 || game.userOperation == "/"){
+    document.getElementById("random1").innerHTML = random1;
+    document.getElementById("random2").innerHTML = random2;
+  } else if (random2 < random1) {
+    document.getElementById("random1").innerHTML = random2;
+    document.getElementById("random2").innerHTML = random1;
+  }
 
 
-// Returns an object with {question, answer}
+}
 
 
-// Checks a user answer
+//function to run new game
+
+function newGame() {
+  random1 = randomGen();
+  random2 = randomGen();
+  game.newQuestion();
+  document.getElementById("answer").value = "";
+  document.getElementById("answer").style.borderColor = "inherit";
+}
+
+//function to check if correct answer - runs new game function if answer is correct
+
+function isCorrectAnswer(userAnswer) {
+  console.log(userAnswer,answer);
+  if (userAnswer == answer) {
+    console.log("You are correct sir.");
+    newGame();
+  }
+  else if (userAnswer != answer){
+    document.getElementById("answer").style.borderColor = "red";
+    runAgain = 0;
+    console.log("Nope, guess again.");
+  }
+
+}
+
+//code to check user answer - run when user submit button is pressed
+
+function checkAnswer() {
+  var userAnswer = document.getElementById('answer').value;
+  isCorrectAnswer(userAnswer);
+}
