0723c2f287324f57dc0f8c72e03fa113e1ddd545
Javier <jvrcueto@outlook.com>
2017-02-06 17:36:31 -0500

    done

--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,13 +1,68 @@
-// Use this file to write the logic of your game, the needed attrs and functions
+function Game(operators, numberLimit) {
+  this.operator = 'addition';
+  this.numberLimit = numberLimit;
+  this.answer = -120000;
+  this.won = false;
+}
 
-var TenSecondsMathGame = function(options) {
-
+Game.prototype.randomNum = function (numberLimit) {
+  return Math.floor(Math.random() * (numberLimit + 1));
 };
 
-// Returns a random integer between [1..numberLimit]
+Game.prototype.validOperation = function (string) {
+  if (string === 'addition'){
+    return true;
+  }
+  return false;
+};
+
+//Ask for game settings
+var operatorInput;
+while(!Game.prototype.validOperation(operatorInput)){
+  operatorInput = prompt('What\'s operator do you want?');
+
+  if (operatorInput == "addition"){
+    break;
+  }
+}
+
+var numLimitInput;
+while(isNaN(numLimitInput)){
+  numLimitInput = prompt('What\'s number do you want?');
+}
+
+numLimitInput = parseInt(numLimitInput);
 
 
-// Returns an object with {question, answer}
+//Create our Game object
+var newGame = new Game(operatorInput, numLimitInput);
+
+//Make out question
+function makeQuestion(game) {
+
+  let n1 = game.randomNum(game.numberLimit);
+  let n2 = game.randomNum(game.numberLimit);
+
+  let operator = game.operator;
+
+  switch(operator) {
+    case 'addition':
+    game.answer = n1 + n2;
+      return 'What is ' + n1 + ' + ' +  n2 + '?';
+  }
+  return "Error";
+}
 
 
-// Checks a user answer
+
+
+//Play the game
+var response = "";
+while(newGame.won === false){
+  response = prompt(makeQuestion(newGame));
+
+  if (response == newGame.answer || response == 'stop') {
+    newGame.won = true;
+    console.log("You did it! You bastard!");
+  }
+}
