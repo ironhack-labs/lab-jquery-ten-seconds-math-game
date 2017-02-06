ae1db0ca1b9e0c0dcf4f6be20a83f7781fc2b36a
Marlon Martinez <jmarlonmartinez13@gmail.com>
2017-02-06 17:33:05 -0500

    done

--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,13 +1,55 @@
-// Use this file to write the logic of your game, the needed attrs and functions
 
-var TenSecondsMathGame = function(options) {
 
+function TenSecondsMathGame(operator,limit){
+this.operator = operator;
+this.limit = limit;
+}
+
+
+
+TenSecondsMathGame.prototype.randomProblem = function () {
+ var randomNumber1 =  Math.floor((Math.random() * this.limit) + 1);
+ var randomNumber2 =  Math.floor((Math.random() * this.limit) + 1);
+ if (this.operator === "+") {
+   this.randomProb = randomNumber1 + randomNumber2;
+   console.log("your problem set is:" + randomNumber1 + "+" +randomNumber2);
+
+ }
+
+ else if (this.operator === "-"){
+   this.randomProb = randomNumber1 - randomNumber2;
+     console.log("your problem set is:" + randomNumber1 + "-" +randomNumber2);
+ }
+
+ else if (this.operator === "/"){
+   this.randomProb = randomNumber1 / randomNumber2;
+     console.log("your problem set is:" + randomNumber1 + "/" +randomNumber2);
+ }
+
+ else if (this.operator === "*") {
+   this.randomProb = randomNumber1 * randomNumber2;
+     console.log("your problem set is:" + randomNumber1 + "*" +randomNumber2);
+ }
+ else if (this.operator === "%") {
+   this.randomProb = randomNumber1 % randomNumber2;
+     console.log("your problem set is:" + randomNumber1 + "%" +randomNumber2);
+ }
+
+ else {
+   console.log("youre very stupid man...");
+ }
 };
 
-// Returns a random integer between [1..numberLimit]
+TenSecondsMathGame.prototype.userAnswer = function (answer) {
+ if (answer != this.randomProb) {
+     console.log("Incorrect!");
+ }
 
+ else {
+   console.log("Correct!");
+ }
+};
 
-// Returns an object with {question, answer}
-
-
-// Checks a user answer
+var randomGame = new TenSecondsMathGame('*', 2);
+randomGame.randomProblem();
+randomGame.userAnswer(2);
