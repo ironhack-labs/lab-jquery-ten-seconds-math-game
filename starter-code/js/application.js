0de42fa9136ad3ce08155e3df4f0b0a5223eb2c6
Fernando Martinez Larrañaga <fernandomartinezlarranaga@MacBook-Air-de-Fernando.local>
2017-04-10 18:37:26 +0200

    done

--- starter-code/js/application.js
+++ starter-code/js/application.js
@@ -6,3 +6,19 @@
 window.onload = function(){
 
 };
+
+function add (n1, n2){
+  return n1 + n2;
+}
+
+function multiply (n1, n2){
+  return n1 * n2;
+}
+
+function divide (n1, n2){
+  return n1 / n2;
+}
+
+function substraction (n1, n2){
+  return n1 - n2;
+}
