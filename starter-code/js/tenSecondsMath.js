4550564a176bbdc98304eeaa57478d8df3dc16b6
TuckerDS <jjperez.tucker.dev@gmail.com>
2017-04-10 17:43:47 +0200

    version1

--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,13 +1,82 @@
-// Use this file to write the logic of your game, the needed attrs and functions
-
-var TenSecondsMathGame = function(options) {
-
+var op = ["+", "-", "*", "/"];
+var TenSecondsMathGame = function(operationsArray, limit) {
+  this.limit = limit;
+  this.operationsArray = operationsArray;
+  this.result = 0;
+  this.timerId;
 };
 
-// Returns a random integer between [1..numberLimit]
+TenSecondsMathGame.prototype.newQuestion = function() {
+  var operation;
+  do {
+    operation = this.generateOperation(this.operationsArray, this.limit);
+    this.result = this.operate(operation);
+  } while (this.result < 0 || (operation.number2 === 0 && operation.operator == "/" ));
+  console.log("Escribe tu respuesta para " +
+  operation.number1 + " " +
+  this.operationsArray[operation.operator]  + " " +
+    operation.number2);
+    this.timer();
+};
 
+TenSecondsMathGame.prototype.isCorrectAnswer = function(answer) {
+    if (answer === this.result) {
+      console.log("You Win !!!");
+      clearTimeout(timerId);
+    } else {
+      console.log("Try Again!");
+    }
+};
 
-// Returns an object with {question, answer}
+TenSecondsMathGame.prototype.operate = function(operation) {
+  var a = operation.number1;
+  var b = operation.number2;
+  var operator = this.operationsArray[operation.operator];
+
+  switch (operator) {
+    case "+":
+      return a+b;
+    case "-":
+      return a-b;
+    case "*":
+      return a*b;
+    case "/":
+      return a/b;
+  }
+};
+
+TenSecondsMathGame.prototype.generateOperation = function (array, lim) {
+  var operation = {
+    operator: this.randomNumber(array.length),
+    number1: this.randomNumber(this.limit+1),
+    number2: this.randomNumber(this.limit+1)
+  };
+
+  return operation;
+};
+
+TenSecondsMathGame.prototype.randomNumber =function (max) {
+  return Math.floor(Math.random() * max);
+};
+
+TenSecondsMathGame.prototype.timer = function(){
+  var counter = 10;
+  var callbackFunction = function () {
+    console.log(counter);
+    timeoutId = setTimeout(callbackFunction, 1000);
+    this.timerId = timeoutId;
+    counter -= 1;
+
+    if (counter <= 0) {
+      clearTimeout(timeoutId);
+      console.log("Time Out, Game Over!!");
+    }
+  };
+  var timeoutId = setTimeout(callbackFunction, 1000);
+};
+
+var game = new TenSecondsMathGame(op, 10);
+game.newQuestion();
 
 
 // Checks a user answer
