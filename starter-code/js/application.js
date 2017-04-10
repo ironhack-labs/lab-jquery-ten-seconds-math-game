69376f19fee1c6137a86fd306c90954d3c3398d5
Darek Glowinski <dariushg32111@gmail.com>
2017-04-10 20:36:47 +0200

    finish

--- starter-code/js/application.js
+++ starter-code/js/application.js
@@ -2,7 +2,93 @@
 
 //Initialize ion library
 
+function showGame() {
+ var destination = $('#position-hook').offset();
+  $('#question-div').css({top: destination.top, left: destination.left});
+  document.getElementById("question-div").style.visibility="visible";
+  document.getElementById("options-div").style.visibility="hidden";
+ 
+  showQuestion()
+  document.getElementById("time-left").textContent = game.timeLeft;
+}
+
+function showQuestion() {
+  document.getElementById("question").textContent = game.question.message;
+}
+
+function timerTick() {
+
+  document.getElementById("time-left").textContent = game.timeLeft;
+}
+
+function startGame() {
+  
+  var operations = [];
+  if(document.getElementById("check-add").checked) operations.push("add");
+  if(document.getElementById("check-sub").checked) operations.push("substract");
+  if(document.getElementById("check-mul").checked) operations.push("multiply");
+  if(document.getElementById("check-div").checked) operations.push("divide");
+  
+  var limit = document.getElementById("number-limit").textContent;
+  console.log(limit);
+  game = new TenSecondsMathGame({ops:operations, numberLimit:limit, gameOverCb:gameOver, timerTickCb:timerTick});
+  
+  game.newQuestion();
+
+  showGame();
+
+ 
+}
+function gameOver() {
+  var destination = $('#position-hook').offset();
+  $('#game-over-div').css({top: destination.top, left: destination.left});
+  document.getElementById("game-over-div").style.visibility="visible";
+  document.getElementById("question-div").style.visibility="hidden";
+}
+
+function showOptions() {
+  document.getElementById("game-over-div").style.visibility="hidden";
+  document.getElementById("options-div").style.visibility="visible";
+}
+
+function checkAnswer(userAnswer) {
+  if( game.isCorrectAnswer(userAnswer) ) {
+    document.getElementById("answer-result").textContent = "Great!"
+    game.newQuestion();
+    showQuestion()
+  } else {
+    document.getElementById("answer-result").textContent = "Wrooong!"
+  }
+  
+  document.getElementById("user-answer").value="";
+  
+  timerTick();
+  setTimeout(function() {
+      document.getElementById("answer-result").textContent="";
+  }, 1000);
+}
+
+var game, timer, timeBonus=10;
 
 window.onload = function(){
 
-};
+  document.getElementById("start-button").onclick=startGame;
+  document.getElementById("options-button").onclick=showOptions;
+
+  document.getElementById("user-answer").onkeypress = function(e) {
+    if(e.keyCode===13) {
+      checkAnswer(document.getElementById("user-answer").value);
+    }
+  }
+  
+
+  var slider = new Slider('#number-limit-input', {
+    tooltip:"hide"
+  });
+
+
+
+  slider.on("slide", function(sliderValue) {
+    document.getElementById("number-limit").textContent = sliderValue;
+  });
+}
--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,13 +1,98 @@
 // Use this file to write the logic of your game, the needed attrs and functions
 
 var TenSecondsMathGame = function(options) {
+    this.operations = options.ops;
+    this.limit = options.numberLimit;
+    this.gameOverCb = options.gameOverCb;
+    this.timerTickCb = options.timerTickCb;
 
-};
-
+    this.timeLeft = timeBonus;
+}
 // Returns a random integer between [1..numberLimit]
-
+TenSecondsMathGame.prototype.randomInLimit = function() {
+    return Math.floor(Math.random()*this.limit) + 1;
+}
 
 // Returns an object with {question, answer}
+TenSecondsMathGame.prototype.generateQuestion = function() {
+    var num1, num2, operation, incorrectQuestion, operationSymbol, answer;
 
+    operation = this.operations[Math.floor(Math.random()*this.operations.length)];
+    
+    do {
+        incorrectQuestion = false;
+ 
+        num1 = this.randomInLimit();
+        num2 = this.randomInLimit();
+
+        switch(operation) {
+            case "add": operationSymbol = " + "; answer=num1+num2; break;
+            case "substract": operationSymbol = " - "; answer=num1-num2; break;
+            case "multiply": operationSymbol = " * "; answer=num1*num2; break;
+            case "divide": operationSymbol = " / "; answer=num1/num2; break;
+        };
+        
+        //rule 1
+        if(answer<0) incorrectQuestion=true;
+        //rule 2
+        if(operation!='divide' && Math.abs(num1-num2)<3)  incorrectQuestion=true;
+        //rule 3
+        if(operation!='divide' && num1===num2)  incorrectQuestion=true;
+        //additional rule for division
+        if(Math.floor(answer)!=answer) incorrectQuestion=true;
+
+    } while(incorrectQuestion);
+
+    return { message: num1+operationSymbol+num2+" =",
+             answer: answer };
+}
 
 // Checks a user answer
+TenSecondsMathGame.prototype.checkAnswer = function(userAnswer) {
+    return parseInt(userAnswer) === this.question.answer;
+}
+
+
+TenSecondsMathGame.prototype.isCorrectAnswer= function(userAnswer) {
+    if(this.checkAnswer(userAnswer)) {
+        console.log("Great!");
+        this.timeLeft+=timeBonus;
+        return true;
+    } else {
+        console.log("Wrooong!");
+        return false;
+    }
+}
+TenSecondsMathGame.prototype.newQuestion = function() {
+    this.question = this.generateQuestion()
+    
+    if(!this.timerID) this.timerID = _startTimer();
+
+    console.log(this.question.message);
+}
+
+TenSecondsMathGame.prototype.gameOver = function () {
+    console.log("Time is out!");
+    clearInterval(this.timerID);
+    this.gameOverCb();
+}
+
+function _startTimer() {
+
+    console.log(game.timeLeft);
+
+    return setInterval(function() {
+        if(game.timeLeft===1) {
+            game.gameOver();
+        } else {
+            game.timeLeft--;
+            game.timerTickCb();
+            console.log(game.timeLeft);
+        }
+    }, 1000);
+}
+
+//var game = new TenSecondsMathGame({ops:["add", "substract", "multiply", "divide"], numberLimit: 10});
+//var game = new TenSecondsMathGame({ops:["add", "substract", "multiply", "divide"], numberLimit: 10});
+
+
