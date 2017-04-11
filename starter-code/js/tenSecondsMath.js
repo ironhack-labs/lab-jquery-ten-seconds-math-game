199a6f97db5a111a35d44b088b7e325bd2efe6f0
Eduard Trias <triaseduard@gmail.com>
2017-04-11 08:59:37 +0200

    TenSecondMaths - try

--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,4 +1,4 @@
-Use this file to write the logic of your game, the needed attrs and functions
+//Use this file to write the logic of your game, the needed attrs and functions
 
 // var TenSecondsMathGame = function(timer) {
 //
@@ -10,49 +10,90 @@ var timer = 10;
 var maxNumber = 10;
 var num1 = 0;
 var num2 = 0;
-//var guess = prompt("what is" + num1 + "+" + num2 + "?");
-//var input = guess;
+
+var input = guess;
 var result;
-var operatorNum = 1;
-var operatorArr = [operator.sum, operator.substract, operator.multiply, operator.divide];
-var operator:{
-  sum: num1 + num2,
-  substract: num1 - num2,
-  multiply: num1 * num2,
-  divide: num1 / num2;
-};
-var operatorAssigned;
+var operatorNum = 1; //number of operators selected
+var points = 0;
+
+//operators
+var sum = [ num1 + num2, { state: false } ];
+var substract = [ num1 - num2, { state: false } ];
+var multiply = [ num1 * num2, { state: false } ];
+var divide = [ num1 / num2, { state: false } ];
+
+var operatorNumber;
+var pickedOperator = operatorArr[operatorNumber];
+
+//operator array
+var operatorArr = [];
 
 // Randomizing the numbers to operate
-
 function randomize(pickedNum) {
+
   num1 = Math.floor(Math.random() * pickedNum + 1);
   console.log(num1);
+
   num2 = Math.floor(Math.random() * pickedNum + 1);
   console.log(num2);
 }
 
 randomize(maxNumber);
 
+var guess = prompt("what is " + num1 + " + " + num2 + "?"); //asking for an imput
+
+//Modify the array of avaliable operators
+function fillOperatorToArr() {
+  if(sum[1].state === true) {
+
+    operatorArr.push(sum[0]);
+
+  } else if (substract[1].state === true) {
+
+    operatorArr.push(substract[0]);
+
+  } else if (multiply[1].state === true) {
+
+    operatorArr.push(multiply[0]);
+
+  } else if (divide[1].state === true) {
+
+    operatorArr.push(divide[0]);
+
+  } else if (sum[1].state === false) {
+
+    //operatorArr.filter(num1 + num2)
+    //have to solve how to delete from the array operations without affecting the rest of the operations.
+
+  }
+}
+
 //Randomizing operator
 function randomizeOperator() {
 
-  if()
+pickedOperator = Math.floor(Math.random() * operatorArr.length);
 
 }
+
 
 //Calculate the result
 function calculateResult() {
-  result = num1 + operatorAssigned + num2;
+  result = pickedOperator;
 }
-
-
-
 
 //Compare the result with the input solution
 function correction() {
-  if (input === ())
+  if (input === result) {
+    counter += 5;
+    points += 0;
+    //clear input field
+    // execute again randomizeOperator();
+  } else {
+    //clear input field
+  }
 }
+
+
 Returns an object with {question, answer}
 
 
@@ -66,46 +107,23 @@ Returns an object with {question, answer}
 
 // Checks a user answer
 
+
+
 //--------setTimeout----------------------
-// var counter = 1;
-// var callbackFunction = function () {
-//   console.log(counter);
-//   timeoutId = setTimeout(callbackFunction, 1000);
-//
-//   counter += 1;
-//
-//   if (counter > 10) {
-//     clearTimeout(timeoutId); // this stops the execution
-//   }
-// }
-//
-// var timeoutId = setTimeout(callbackFunction, 1000);
-// //still don't know how to stop this
-//
-//
-// //----------------setInterval-------------------------
-//
-// var i = 1;
-// var intervalId = setInterval(function () {
-//   console.log(i);
-//
-//   i++;
-//
-//   if (i > 10) { // when i > 10 the function stops
-//     clearInterval(intervalId);
-//   }
-// }, 1000); // delay time between executions
-//
-// // Same exam for a countdown interval
-//
-// var i = 10;
-// var intervalId = setInterval(function() {
-//   if (i > 0) {
-//     console.log(i);
-//   } else {
-//     console.log("Pop!");
-//     clearInterval(intervalId);
-//   }
-//
-//   i--;
-// }, 1000);
+
+var counter = 10;
+var callbackFunction = function () {
+  console.log(counter);
+  timeoutId = setTimeout(callbackFunction, 1000);
+
+  counter -= 1;
+
+  if (counter === 0) {
+    clearTimeout(timeoutId); // this stops the execution
+     //pop up message Game Over
+  }
+}
+
+var timeoutId = setTimeout(callbackFunction, 1000);
+//still don't know how to stop this
+*/
