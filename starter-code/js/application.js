595c27cc7c19b5e384ddd0b0db34ed508316237d
Juanma Perez <juanmaperezvar@gmail.com>
2017-06-01 17:14:46 +0200

    done

--- starter-code/js/application.js
+++ starter-code/js/application.js
@@ -2,7 +2,38 @@
 
 //Initialize ion library
 
+$(document).ready(function(){
 
-window.onload = function(){
+  $('.operator').on("click", function(){
+    var maxNum = $('.maxNums').val();
+    var operation = $(this).val()
 
-};
+    if (maxNum > 1){
+      var game = new TenSecondsMathGame(operation, maxNum);
+      game.printQuestion();
+
+
+      $('.send').on("click", function(){
+        game.checkAnswer();
+      });
+
+      var counting = setInterval(function(){
+        game.decreaseCounter();
+
+        $('.progress-bar').css("width", game.counter + "%");
+        $('.progress-bar').text(game.counter + "%");
+
+        game.checkCounter();
+
+      },1000);
+
+    }else {
+      $(".maxNums").addClass("error");
+    }
+  });
+
+
+
+
+
+});
