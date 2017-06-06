b36b0f2dead263f831eec911fd223bc27b881aa5
Julia Gottschalk <giulia.gottschalk@gmail.com>
2017-06-06 19:05:36 +0200

    done

--- starter-code/js/application.js
+++ starter-code/js/application.js
@@ -1,8 +1,135 @@
-// Use this file to write the interactions between your game and the user
 
-//Initialize ion library
+ion.sound({
+  sounds: [
+    {
+      name: "cash-register"
+    }
+  ],
+  volume: 1,
+  path: "audios/",
+  preload: true
+});
+
+function getSettings(rangeInput){
+
+    var checkboxes = $('[type=checkbox]');
+
+    var checkBoxTrue=[];
+    Array.from(checkboxes).forEach(function( elem ){
+
+        if(elem.checked === true){
+          checkBoxTrue.push(elem);
+
+        }
+
+    });
+    if ((checkBoxTrue.length > 1) || (checkBoxTrue.length < 1)){
+      var myOperator = checkboxes[Math.floor(Math.random() * checkboxes.length)].id;
+
+    }
+    else if(checkBoxTrue.length = 1){
+      var myOperator = checkBoxTrue[0].id;
+    }
+    TenSecondsMathGame.makeMathChallenge(myOperator, rangeInput, function(mathTask){
+
+        $('#gamesettings').hide();
+        $('#math-task').text(mathTask);
+        $('#gogame').show();
+
+    });
+}
+
+function startClock(){
+  var CountDown = TenSecondsMathGame.timeLeft + 10;
+  TenSecondsMathGame._startTimer(CountDown);
+}
 
 
-window.onload = function(){
+function displayCountdown(CountDown){
+  var timerWrapper = document.getElementById('timerwrapper');
+  var timerElement = document.getElementById('timer');
 
-};
+  if(CountDown <= 0){
+      $('#gameover-img').show();
+      timerWrapper.style.display = "none";
+      document.getElementById('userinput').disabled = true;
+      document.getElementById('restart').style.display = "block";
+  }
+  else if(CountDown > 0){
+
+    timerElement.innerHTML=CountDown;
+    timerWrapper.style.display = "block";
+
+  }
+}
+
+function reStartGame(){
+  $('#gogame').hide();
+  $('#gameover-img').hide();
+  $('#gamesettings').show();
+  $('#restart').hide();
+  document.getElementById('userinput').value = "";
+  document.getElementById('userinput').disabled = false;
+
+}
+
+function hideCoin(){
+  setTimeout(function(){
+    document.getElementById('gamecontainer').style.visibility="hidden";
+  },1000);
+}
+
+
+function checkUserAnswer(userAnswer){
+
+  TenSecondsMathGame.isCorrectAnswer(userAnswer, function(userAnswer){
+      if(userAnswer){
+
+        var positionx = Math.floor(Math.random() * 350) + "px";
+        var positiony = Math.floor(Math.random() * 50) + "px";
+        document.getElementById('coin-img').style.position="absolute";
+        document.getElementById('coin-img').style.left = positionx;
+        document.getElementById('coin-img').style.top = positiony;
+        document.getElementById('gamecontainer').style.visibility="visible";
+
+        ion.sound.play("cash-register");
+        document.getElementById('userinput').value="";
+        hideCoin();
+        TenSecondsMathGame.makeMathChallenge("", "", function(mathTask){
+          $('#math-task').text(mathTask);
+          clearInterval(interval);
+          startClock();
+        });
+      }else if(!userAnswer){
+        $('#gameover-img').show();
+        var timerWrapper = document.getElementById('timerwrapper');
+        clearInterval(interval);
+        timerWrapper.style.display = "none";
+        document.getElementById('userinput').disabled = true;
+        document.getElementById('restart').style.display = "block";
+      }
+    });
+
+}
+
+document.addEventListener("DOMContentLoaded", function(event){
+  var rangeInput = $('#range-value').val();
+  $('#userinput').on('blur', function(){
+      var userAnswer = $('#userinput').val();
+      checkUserAnswer(userAnswer);
+  });
+
+  $('#range-value').on('change', function(){
+    rangeInput = $('#range-value').val();
+    $('#current-range-value').text(rangeInput);
+  });
+  $('#current-range-value').text(rangeInput);
+  $('#startgame').on('click', function(){
+      getSettings(rangeInput);
+      startClock();
+  });
+  $('#restart').on('click', function(){
+      reStartGame();
+  })
+
+});
--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,13 +1,126 @@
-// Use this file to write the logic of your game, the needed attrs and functions
 
-var TenSecondsMathGame = function(options) {
+var TenSecondsMathGame = {
+    computerResult : 0,
+    selectedOperator: "",
+    selectedRangeOfInput: "",
+    timeLeft: 0,
 
-};
+    generateRandomNumbers: function(rangeOfNumbersInput){
+      var numbers=[];
+      var number1 = Math.floor(Math.random() * rangeOfNumbersInput + 1);
+      numbers.push(number1);
+      var number2 = Math.floor(Math.random() * rangeOfNumbersInput + 1);
+      numbers.push(number2);
+      return numbers;
+    },
 
-// Returns a random integer between [1..numberLimit]
+    makeMathChallenge: function(operator, rangeOfNumbersInput, callback){
 
+      if(rangeOfNumbersInput != ""){
+        this.selectedRangeOfInput=rangeOfNumbersInput;
+      }
 
-// Returns an object with {question, answer}
+      if(rangeOfNumbersInput == ""){
 
+        rangeOfNumbersInput=this.selectedRangeOfInput;
+      }
+      var selectedRangeOfNumbers=rangeOfNumbersInput;
+      if (operator == ""){
+          operator=TenSecondsMathGame.selectedOperator;
+      }
+      if (rangeOfNumbersInput === ""){
+        rangeOfNumbersInput = selectedRangeOfNumbers;
+      }
 
-// Checks a user answer
+      var numbers=TenSecondsMathGame.generateRandomNumbers(rangeOfNumbersInput);
+      var number1=numbers[0];
+      var number2=numbers[1];
+      var mathTask;
+      switch(operator) {
+        case 'addition' :
+            mathTask = number1 + " + " + number2;
+            this.selectedOperator = "addition";
+            TenSecondsMathGame.computerResult = number1 +  number2;
+          break;
+        case 'substraction' :
+        this.selectedOperator = "substraction";
+          if((number1 > number2) || (number1 === number2)){
+            mathTask = number1 + " - " + number2;
+            TenSecondsMathGame.computerResult = number1 - number2;
+          }
+          else if(number1 < number2){
+              mathTask = number2 + " - " + number1;
+            TenSecondsMathGame.computerResult = number2 - number1;
+          }
+          break;
+        case 'multiplication' :
+          this.selectedOperator = "multiplication";
+          mathTask = number1 + " * " + number2;
+            TenSecondsMathGame.computerResult = number1 * number2;
+          break;
+        case 'division' :
+          this.selectedOperator = "division";
+          mathTask = number1 + " : " + number2;
+          TenSecondsMathGame.computerResult = number1 / number2;
+          break;
+
+        }
+
+      mathTask+= " = ";
+      return callback(mathTask);
+    },
+
+    isCorrectAnswer: function(userAnswer, callback){
+
+      if(TenSecondsMathGame.computerResult == userAnswer){
+          userAnswer = true;
+
+      }
+      else{
+          userAnswer = false;
+      }
+      return callback(userAnswer);
+  },
+
+    _checkTime: function(CountDown){
+      TenSecondsMathGame.timeLeft = CountDown;
+    },
+
+    _startTimer: function(CountDown){
+
+      interval = setInterval(function(){
+        displayCountdown(CountDown);
+        TenSecondsMathGame._checkTime(CountDown);
+        CountDown--;
+        if (CountDown < 0){
+          clearInterval(interval); // Stopping the counter when reaching 0.
+        }
+      }, 1000);
+    },
+
+    giveFeedbackToUser: function(userAnswer){
+      if(userAnswer){
+
+      }
+      else if(!userAnswer){
+
+      }
+    },
+
+    getMathOperators : function(operator){
+      switch(operator) {
+        case addition :
+          break;
+        case substraction :
+          break;
+        case multiplication :
+          break;
+        case division :
+          break;
+        default:
+
+      }
+
+    }
+
+}
