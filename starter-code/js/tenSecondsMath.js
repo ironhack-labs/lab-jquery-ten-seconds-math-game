e6afddca227f2b022811daf1348bc38655682573
salvocostahack <salvatorecostanzopa@gmail.com>
2017-06-19 17:15:48 +0200

    first iteration done

--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,33 +1,94 @@
 // Use this file to write the logic of your game, the needed attrs and functions
 
 var TenSecondsMathGame = function(limit, operation) {
-      this.operation = operation;
-      this.limit = limit;
-      this.firstNumber = this.randomNumber(limit);
-      this.secondNumber = this.randomNumber(limit);
-      this.result = 0;
+  this.operation = operation;
+  this.limit = limit;
+  this.firstNumber = 0;
+  this.secondNumber = 0;
+  this.result = 0;
 };
 
-TenSecondsMathGame.prototype.randomNumber = function (limit) {
-var random =  Math.floor((Math.random() * limit) + 1 );
+TenSecondsMathGame.prototype.randomNumber = function(limit) {
+  var random = Math.floor((Math.random() * limit) + 1);
   return random;
 };
 
-var game = new TenSecondsMathGame(8, "sum");
 
+TenSecondsMathGame.prototype.isCorrectAnswer = function(answer) {
+  if (this.result === answer) {
+    console.log("Great");
+    this.generateQuestion();
+
+    this._checkTimer();
+  } else {
+    console.log("Wroooong!!!");
+  }
+};
+
+TenSecondsMathGame.prototype._checkTimer = function() {
+  var i = 10;
+  var intervalId = setInterval(function() {
+    console.log(i);
+    i--;
+    if (i < 0) {
+      clearInterval(intervalId);
+      // "Time is over" is printed out 1seg after in the console
+      setTimeout(function() { console.log('Time is over'); }, 1000);
+    }
+  }, 1000);
+
+  return intervalId;
+};
 // console.log(game.__proto__.randomNumber(10));
 
-TenSecondsMathGame.prototype.generateQuestion = function () {
-    switch (this.operation) {
-      case "sum":
+TenSecondsMathGame.prototype.generateQuestion = function() {
+
+  this.firstNumber = this.randomNumber(this.limit);
+  this.secondNumber = this.randomNumber(this.limit);
+
+  switch (this.operation) {
+    case "sum":
+      if (this.firstNumber === this.secondNumber) {
+        this.firstNumber += 1;
+      }
       console.log(this.firstNumber + " + " + this.secondNumber);
       this.result = this.firstNumber + this.secondNumber;
       break;
-    }
-    return this.result;
-  };
 
-  console.log(game.generateQuestion());
+    case "sub":
+      if (this.firstNumber === this.secondNumber) {
+        this.firstNumber += 1;
+      }
+      if (this.firstNumber < this.secondNumber) {
+        var aux = this.firstNumber;
+        this.firstNumber = this.secondNumber;
+        this.secondNumber = aux;
+      }
+      this.result = this.firstNumber - this.secondNumber;
+      console.log(this.firstNumber + " - " + this.secondNumber);
+      break;
+
+    case "multiply":
+      if (this.firstNumber === this.secondNumber) {
+        this.firstNumber += 1;
+      }
+      console.log(this.firstNumber + " * " + this.secondNumber);
+      this.result = this.firstNumber * this.secondNumber;
+      break;
+
+    case "divide":
+      if (this.firstNumber < this.secondNumber) {
+        var aux2 = this.firstNumber;
+        this.firstNumber = this.secondNumber;
+        this.secondNumber = aux2;
+      }
+      console.log(this.firstNumber + " / " + this.secondNumber);
+      this.result = this.firstNumber / this.secondNumber;
+      break;
+  }
+};
+
+//
 
 
 // Returns a random integer between [1..numberLimit]
