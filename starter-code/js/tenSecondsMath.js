d88b61e7fcda7c6765ace5441a6f4ab9b91a8c6c
Alejandro de la Fuente <alex695afr@gmail.com>
2017-06-19 13:14:55 +0200

    In process

--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,9 +1,75 @@
 // Use this file to write the logic of your game, the needed attrs and functions
 
-var TenSecondsMathGame = function(options) {
+var TenSecondsMathGame = function(operations, limit) {
+  this.operations = operations;
+  this.limit = operations;
 
+  this.operations = prompt("Introduce las operaciones,Suma -> s Resta -> r Multiplicación -> m División ->D").toUpperCase();
+  this.limit = prompt("Introduce el limite numerico");
+  console.log(operations);
+
+
+
+  /*for (var i = 0; i < operations.length; i++) {
+    if (true) {
+
+    }
+    if (true) {
+
+    }
+    if (true) {
+
+    }
+    if (true) {
+
+    }
+  }
+  */
 };
 
+
+function questionGenerator() {
+
+
+  switch (operations) {
+    case 'S':
+
+      addition();
+
+      break;
+    case 'R':
+      subtraction();
+
+      break;
+    case 'M':
+      multi();
+
+      break;
+    case 'D':
+      division();
+
+      break;
+    default:
+
+
+  }
+
+  function addition() {
+    var sum1 = 0;
+    var sum2 = 0;
+
+    sum1 = Math.floor(Math.random() * (limit.TenSecondsMathGame - min)) + limit.TenSecondsMathGame1;
+    sum2 = Math.floor(Math.random() * (limit.TenSecondsMathGame - min)) + limit.TenSecondsMathGame1;
+    return console.log(sum1 + " + " + sum2);
+
+
+  }
+
+  function subtraction() {
+
+  }
+
+}
 // Returns a random integer between [1..numberLimit]
 
 
