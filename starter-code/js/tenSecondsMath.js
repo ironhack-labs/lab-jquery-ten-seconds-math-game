1f4fa0a4e77cd092a042ddc1d955761b99b52560
Monica Calderaro <monica.calderaro@sortis.es>
2017-06-19 13:13:36 +0200

    not much :(

--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,13 +1,27 @@
 // Use this file to write the logic of your game, the needed attrs and functions
-
 var TenSecondsMathGame = function(options) {
-
+  this.n1 = 0;
+  this.n2 = 0;
+  this.limit = Math.floor(prompt("What's your number limit?"));
+  this.operation = prompt("Which operations you want?");
 };
 
 // Returns a random integer between [1..numberLimit]
-
+TenSecondsMathGame.prototype.randomNum = function (limit) {
+  return Math.floor(Math.random() * limit);
+};
 
 // Returns an object with {question, answer}
 
+TenSecondsMathGame.prototype.generateBothNum = function () {
+  this.n1 = tenSecondsMath.randomNum(this.limit);
+  this.n2 = tenSecondsMath.randomNum(this.limit);
+  
+}
+
+
 
 // Checks a user answer
+var tenSecondsMath = new TenSecondsMathGame();
+
+// tenSecondsMath.randomNum(this.limit);
