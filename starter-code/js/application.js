521a3aa8a872c4127feda3bfe73ba898f45edc77
Daniele <daniele.germani.85@gmail.com>
2017-06-19 17:21:19 +0200

    just mathRandom

--- starter-code/js/application.js
+++ starter-code/js/application.js
@@ -1,8 +1,12 @@
 // Use this file to write the interactions between your game and the user
 
 //Initialize ion library
-
+var game;
 
 window.onload = function(){
-
+  game = new TenSecondsMathGame("+",20);
+    game.chooseNumber();
+    game.chooseOperator();
+  var result = prompt(game.n1+ game.operator +game.n2);
+  game.checkResult(result);
 };
--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,8 +1,52 @@
 // Use this file to write the logic of your game, the needed attrs and functions
 
-var TenSecondsMathGame = function(options) {
+var TenSecondsMathGame = function( operator, numberLimit) {
+  this.n1 = 0;
+  this.n2 = 0;
+  this.operator = operator;
+  this.numberLimit = numberLimit;
+  this.result = 0;
+};
+TenSecondsMathGame.prototype.chooseNumber = function() {
+  this.n1 = Math.floor(Math.random() * this.numberLimit );
+  this.n2 = Math.floor(Math.random() * this.numberLimit );
+};
+TenSecondsMathGame.prototype.chooseOperator = function() {
+  switch (this.operator) {
+    case "+":
+      this.result = this.n1 + this.n2;
+
+      break;
+    case "-":
+      this.result = this.n1 - this.n2;
+      break;
+    case "/":
+    this.result = this.n1 / this.n2;
+      break;
+      case "*":
+      this.result = this.n1 * this.n2;
+
+        break;
+    default:
+    console.log("choose a valid operation!!!please!!!");
+
+
+  }
+
+TenSecondsMathGame.prototype.checkResult = function(result){
+  if(this.result === this.n1 + this.n2){
+    prompt(this.n1 + this.n2);
+
+  }
+
 
 };
+
+};
+
+
+
+
 
 // Returns a random integer between [1..numberLimit]
 
