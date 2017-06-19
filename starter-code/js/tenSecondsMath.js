69b7e4520d9804c0825ab0ca2aa206334c73cdcb
Ana Milojevic <anamilojevicd@gmail.com>
2017-06-19 13:16:51 +0200

    wip

--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,8 +1,20 @@
 // Use this file to write the logic of your game, the needed attrs and functions
 
-var TenSecondsMathGame = function(options) {
+var TenSecondsMathGame = function(operator, limit){
+  this.operator = ["+"],
+  this.limit = limit,
+  newQuestion = function(){},
+  isCorrectAnswer = function(){}
+}
 
-};
+// function getNum(n){
+//   var num = Math.floor(Math.random() * n) + 1;
+//   return num;
+// }
+
+// function newQuestion(){
+// }
+
 
 // Returns a random integer between [1..numberLimit]
 
