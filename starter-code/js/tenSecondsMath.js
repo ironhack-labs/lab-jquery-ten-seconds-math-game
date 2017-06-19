13c75aef7840f4fa3df8a3020227692330182dd3
Fernando Palma <ferpalma21@gmail.com>
2017-06-19 16:20:30 +0200

    finished

--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,19 +1,15 @@
 // Use this file to write the logic of your game, the needed attrs and functions
+var remainTime = 10;
+
 
 var TenSecondsMathGame = function(operation, upperNumber) {
-  var n1 = this.random(upperNumber);
-  var n2 = this.random(upperNumber);
-  console.log(n1, n2);
-  var userSay = prompt("What's the result?");
-  var result = this.result(operation, n1, n2);
-  if(result == userSay){
-    console.log("CONGRATS!!!");
-  }else{
-    console.log("LOOSER!!!");
-  }
-  console.log(n1, n2, result);
+  this.operation = operation;
+  this.upperNumber = upperNumber;
+  this.generateQuestion();
+
 
 };
+
 TenSecondsMathGame.prototype.random = function(upperNumber) {
   return Math.floor(Math.random() * upperNumber + 1);
 };
@@ -21,22 +17,59 @@ TenSecondsMathGame.prototype.random = function(upperNumber) {
 TenSecondsMathGame.prototype.result = function(operation, n1, n2) {
   var operationResult = 0;
   switch (operation) {
-    case 'add':
+    case '+':
       operationResult = n1 + n2;
       break;
-    case 'subs':
-      operationResult = n1 - n2;
+    case '-':
+    operationResult = n1 - n2;
       break;
-    case 'mult':
+    case '*':
       operationResult = n1 * n2;
       break;
-    case 'div':
+    case '/':
       operationResult = n1 / n2;
       break;
   }
-  console.log(operationResult);
   return operationResult;
 };
+
+TenSecondsMathGame.prototype.generateQuestion = function () {
+  this.n1 = this.random(this.upperNumber);
+  this.n2 = this.random(this.upperNumber);
+  if (this.n1 < this.n2) {
+    var aux = this.n1;
+this.n1 = this.n2;
+    this.n2 = aux;
+  }
+  console.log( this.n1 + " " + this.operation + " " + this.n2);
+   
+
+};
+
+TenSecondsMathGame.prototype.checkAnswer = function (userSay) {
+  var result = this.result(this.operation, this.n1, this.n2);
+  if(result == userSay){
+    console.log("CONGRATS!!!");
+    remainTime += 10;
+    this.generateQuestion();
+  }else{
+
+  }
+
+};
+
+var intervalId = setInterval(function(){
+  if (remainTime === 0){
+    console.log("LOOSER!!!");
+    clearInterval(intervalId);
+  }else {
+    console.log(remainTime);
+  remainTime--;
+  }
+},1 * 1000);
+
+var game = new TenSecondsMathGame('+',10);
+
 
 
 // Returns a random integer between [1..numberLimit]
