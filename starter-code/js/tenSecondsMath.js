d2aadbc423422bff89cfde44bc608b0f4d2a5b4b
salvocostahack <salvatorecostanzopa@gmail.com>
2017-06-19 13:06:27 +0200

    first commit

--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,8 +1,28 @@
 // Use this file to write the logic of your game, the needed attrs and functions
 
-var TenSecondsMathGame = function(options) {
-
+var TenSecondsMathGame = function(limit, operation) {
+      this.operation = operation;
+      this.limit = limit;
+      this.firstNumber = this.randomNumber(limit);
+      this.secondNumber = this.randomNumber(limit);
 };
+
+TenSecondsMathGame.prototype.randomNumber = function (limit) {
+var random =  Math.floor((Math.random() * limit) + 1 );
+  return random;
+};
+
+var game = new TenSecondsMathGame(8, "sum");
+
+// console.log(game.__proto__.randomNumber(10));
+
+TenSecondsMathGame.prototype.generateQuestion = function () {
+
+    return this.firstNumber + this.secondNumber;
+  };
+
+  console.log(game.generateQuestion());
+
 
 // Returns a random integer between [1..numberLimit]
 
