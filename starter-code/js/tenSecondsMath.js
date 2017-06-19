701545730700e5b8939a283c793bcdf96499148c
ferrodri <f.rodriguez.hervias@gmail.com>
2017-06-19 13:22:39 +0200

    pair Ferrodri y rosa

--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,9 +1,48 @@
-// Use this file to write the logic of your game, the needed attrs and functions
+
 
 var TenSecondsMathGame = function(options) {
 
 };
 
+function add(n1,n2){
+  return n1+n2;
+}
+
+function substraction(n1,n2){
+  return n1-n2;
+}
+
+function multiply(n1,n2){
+  return n1*n2;
+}
+
+function division(n1,n2){
+  return n1/n2;
+}
+function randomNumber(maxNumber){
+  return Math.floor(Math.random()*maxNumber);
+}
+
+function generateOperation(n1,n2,operator){
+  if (operator==="+"){
+    console.log("El resultado de la suma " + n1 + " " + operator + " " + n2 + " es " + add(n1,n2));
+  }
+}
+var maxNumber;
+function selectMaximum() {
+  maxNumber = parseInt(prompt("Select your maximum Number!"));
+  return maxNumber;
+}
+// Hilo principal
+
+var operator = "+";
+
+selectMaximum();
+
+var n1 = randomNumber(maxNumber);
+var n2 = randomNumber(maxNumber);
+
+generateOperation(n1,n2,operator);
 // Returns a random integer between [1..numberLimit]
 
 
