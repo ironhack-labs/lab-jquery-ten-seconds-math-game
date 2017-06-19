e6f903f6caadad603fcb0370c8161c6824b95fb9
Fernando Palma <ferpalma21@gmail.com>
2017-06-19 13:22:50 +0200

    first iteration

--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,8 +1,43 @@
 // Use this file to write the logic of your game, the needed attrs and functions
 
-var TenSecondsMathGame = function(options) {
+var TenSecondsMathGame = function(operation, upperNumber) {
+  var n1 = this.random(upperNumber);
+  var n2 = this.random(upperNumber);
+  console.log(n1, n2);
+  var userSay = prompt("What's the result?");
+  var result = this.result(operation, n1, n2);
+  if(result == userSay){
+    console.log("CONGRATS!!!");
+  }else{
+    console.log("LOOSER!!!");
+  }
+  console.log(n1, n2, result);
 
 };
+TenSecondsMathGame.prototype.random = function(upperNumber) {
+  return Math.floor(Math.random() * upperNumber + 1);
+};
+
+TenSecondsMathGame.prototype.result = function(operation, n1, n2) {
+  var operationResult = 0;
+  switch (operation) {
+    case 'add':
+      operationResult = n1 + n2;
+      break;
+    case 'subs':
+      operationResult = n1 - n2;
+      break;
+    case 'mult':
+      operationResult = n1 * n2;
+      break;
+    case 'div':
+      operationResult = n1 / n2;
+      break;
+  }
+  console.log(operationResult);
+  return operationResult;
+};
+
 
 // Returns a random integer between [1..numberLimit]
 
