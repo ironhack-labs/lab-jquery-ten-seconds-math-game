845cc188dc644380482d72955b72fff6dd257ffa
sara <sara-cg@github.com>
2017-06-19 17:07:09 +0200

    progress

--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,29 +1,70 @@
 // Use this file to write the logic of your game, the needed attrs and functions
 
-var ops = ["+", "-", "+", "/"];
-var operator = ops[i];
-var TenSecondsMathGame = function(limit, operator) {
-  if (operator.lenght > 1) {
-    var random = Math.floor(Math.random() * operator.lenght);
-    switch (random) {
-      case random = 1:
-        operator = "+";
-        break;
-      case random = 2:
-        operator = "-";
-        break;
-      case random = 2:
-        operator = "*";
-        break;
-      case random = 2:
-        operator = "/";
-        break;
-    }
-  }
-  var n1 = Math.floor(Math.random() * limit + 1);
-  var n2 = Math.floor(Math.random() * limit + 1);
-  var mathQuestion = n1 + " " + operator + " " + n2;
+function Calculator(limitNumber, operator) {
+  this.limitNumber = limitNumber;
+  this.numberOne = 0;
+  this.numberTwo = 0;
+  this.operator = operator;
+}
+
+Calculator.prototype.randomNumber = function() {
+  return Math.floor(Math.random() * this.limitNumber + 1);
 };
+
+Calculator.prototype.checkAbsNumbers = function(){
+  return Math.abs((this.numberOne - this.numberTwo) < 3);
+};
+
+Calculator.prototype.generateBothRandomNumber = function(){
+  do {
+  this.numberOne = this.randomNumber();
+  this.numberTwo = this.randomNumber();
+} while ( (this.numberOne === this.numberTwo) || this.checkAbsNumbers());
+};
+
+Calculator.prototype.chooseOperation = function() {
+  switch (this.operator) {
+    case "+":
+    this.result = this.numberOne + this.numberTwo;
+      break;
+    case "-":
+    if (this.numberOne >= this.numberTwo) {
+    this.result = this.numberOne - this.numberTwo;
+  } else {
+    this.result = this.numberTwo - this.numberOne;
+  }
+      break;
+    case "*":
+    this.result = this.numberOne * this.numberTwo;
+      break;
+    case "/":
+    this.result = his.numberOne / this.numberTwo;
+      break;
+  }
+  return this.result;
+};
+
+//if (this.operator[i] > 1)
+Calculator.prototype.randomOperation = function() {
+    var randomOperator = Math.floor(Math.random() * this.operator.lenght);
+    switch (randomOperator) {
+      case randomOperator = 1:
+    this.result = this.numberOne + this.numberTwo;
+        break;
+      case randomOperator = 2:
+    this.result = this.numberOne - this.numberTwo;
+        break;
+      case randomOperator = 3:
+    this.result = this.numberOne * this.numberTwo;
+        break;
+      case randomOperator = 4:
+    this.result = his.numberOne / this.numberTwo;
+        break;
+  }
+    return this.result;
+};
+
+var TenSecondsMathGame = function(limit, operator) {};
 
 // Returns a random integer between [1..numberLimit]
 
