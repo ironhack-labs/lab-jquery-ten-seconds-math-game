c179f48db0c584eba3a53feb8afc2924a14b2f3b
Victor Rodriguez <victor.irix@gmail.com>
2017-06-19 13:05:03 +0200

    Mid Morning

--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,13 +1,59 @@
 // Use this file to write the logic of your game, the needed attrs and functions
+var op = [];
+var operation;
+var max;
 
-var TenSecondsMathGame = function(options) {
+function init() {
+  operation = prompt("Select one type of operation: +, -, * or /");
+  var firstAnswer;
+  if (operation != "+" && operation != "-" && operation != "*" && operation != "/" ){
+    firstAnswer = false;
+  } else { firstAnswer = true;
+    op.push(operation);
+    return;
+  }
 
-};
+    while (firstAnswer === false) {
+      prompt("Selection must be + , - , * , /");
+      if (operation !== "+" && operation !== "-" && operation !== "*" && operation !== "/" ){
+        firstAnswer = false;
+      } else { firstAnswer = true;
+        op.push(operation);
+      }
+}}
 
-// Returns a random integer between [1..numberLimit]
+function setMax(){
+  max = prompt ("Tipe the maximun number");
+}
+
+function generateNumbers(){
+  var x = Math.floor(Math.random * max);
+  var y = Math.floor(Math.random * max);
+  return [x,y];
+}
+
+function getRandomOperator (){
+  var
+}
+
+/*function addOperations (){
+  var continueAnswer = true;
+  while (continueAnswer === true) {ç
+
+        var continueAnswer = prompt("Do you want another operation?");
+    if (continueAnswer === false) {
+      return;
+    }
+}
+}*/
+
+//var TenSecondsMathGame = function(operationsArray, limit) {
 
 
-// Returns an object with {question, answer}
+    // Returns a random integer between [1..numberLimit]
 
 
-// Checks a user answer
+    // Returns an object with {question, answer}
+
+
+    // Checks a user answer
