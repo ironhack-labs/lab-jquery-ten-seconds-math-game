311342524098aa843640686f8e194c913a6b9008
ferrodri <f.rodriguez.hervias@gmail.com>
2017-06-20 09:29:43 +0200

    última entrega

--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,66 +1,127 @@
-
-
 var TenSecondsMathGame = function() {
-  this.n1 = 3;
-  this.n2 = 1;
-  this.maxNumber = 20;
-
+  this.n1 = 0;
+  this.n2 = 0;
+  this.maxNumber = 30;
+  this.solution = 0;
+};
+TenSecondsMathGame.prototype.final = function() {
+  operation = prompt("Enter the operation: add, substraction, multiply, division");
+  switch (operation) {
+    case "add":
+      this.add();
+      break;
+    case "substraction":
+      this.substraction();
+      break;
+    case "multiply":
+      this.multiply();
+      break;
+    case "division":
+      this.division();
+      break;
+    default:
+      console.log("this is not a correct operation");
+      return this.final();
+  }
 };
 
-TenSecondsMathGame.prototype.add = function (){
-  if(this.comprobation() === true) {
+
+TenSecondsMathGame.prototype.add = function() {
+  this.generateTwoRandomNumbers();
+  if (this.comprobation() === true) {
     this.generateTwoRandomNumbers();
     return this.add();
-  }
-  else {
-    return this.n1+this.n2;
+
+  } else {
+    console.log(this.n1 + "+" + this.n2);
+    this.solution = parseInt(prompt("Enter the correct answer"));
+    if (this.solution == (this.n1 + this.n2)) {
+      console.log("Great!");
+      this.add();
+    } else {
+      console.log("Wrooong!");
+    }
   }
 };
 
-TenSecondsMathGame.prototype.substraction = function(){
-  if((this.n1<this.n2) || (this.comprobation() === true)) {
+TenSecondsMathGame.prototype.substraction = function() {
+  this.generateTwoRandomNumbers();
+  if ((this.n1 < this.n2) || (this.comprobation() === true)) {
     this.generateTwoRandomNumbers();
     return this.substraction();
-  }
-  else {
-    return this.n1-this.n2;
+  } else {
+    console.log(this.n1 + "-" + this.n2);
+    this.solution = parseInt(prompt("Enter the correct answer"));
+    if (this.solution == (this.n1 - this.n2)) {
+      console.log("Great!");
+      this.substraction();
+    } else {
+      console.log("Wrooong!");
+    }
   }
 };
 
-TenSecondsMathGame.prototype.multiply = function(){
-  if(this.comprobation() === true) {
+TenSecondsMathGame.prototype.multiply = function() {
+  this.generateTwoRandomNumbers();
+  if (this.comprobation() === true) {
     this.generateTwoRandomNumbers();
     return this.multiply();
+  } else {
+    console.log(this.n1 + "*" + this.n2);
+    this.solution = parseInt(prompt("Enter the correct answer"));
+    if (this.solution == (this.n1 * this.n2)) {
+      console.log("Great!");
+      this.multiply();
+    } else {
+      console.log("Wrooong!");
+    }
   }
-  else {
-  return this.n1*this.n2;
+};
+
+TenSecondsMathGame.prototype.division = function() {
+  this.generateTwoRandomNumbers();
+  console.log(this.n1 + "/" + this.n2);
+  this.solution = parseInt(prompt("Enter the correct answer"));
+  if (this.solution == (this.n1 / this.n2)) {
+    console.log("Great!");
+    this.division();
+  } else {
+    console.log("Wrooong!");
   }
 };
 
-TenSecondsMathGame.prototype.division = function(){
-  return this.n1/this.n2;
+TenSecondsMathGame.prototype.randomNumber = function() {
+  return Math.floor(Math.random() * this.maxNumber);
 };
 
-TenSecondsMathGame.prototype.randomNumber = function(){
-  return Math.floor(Math.random()*this.maxNumber);
-};
-
-TenSecondsMathGame.prototype.generateTwoRandomNumbers = function(){
+TenSecondsMathGame.prototype.generateTwoRandomNumbers = function() {
   this.n1 = this.randomNumber();
   this.n2 = this.randomNumber();
 };
 
-TenSecondsMathGame.prototype.setMaxNumber = function(maxNumber){
+TenSecondsMathGame.prototype.setMaxNumber = function(maxNumber) {
   this.maxNumber = maxNumber;
 };
 
-TenSecondsMathGame.prototype.comprobation = function () {
-  if((-4 < (this.n1-this.n2)) && ((this.n1-this.n2)<4)) {
+TenSecondsMathGame.prototype.comprobation = function() {
+  if ((-4 < (this.n1 - this.n2)) && ((this.n1 - this.n2) < 4)) {
     return true;
   } else {
     return false;
   }
 };
+
+  var i = 10;
+  var intervalId = setInterval(function() {
+    if (i > 0) {
+      console.log(i);
+    } else {
+      console.log("Pop!");
+      clearInterval(intervalId);
+    }
+
+    i--;
+  }, 1000)
 
 
 
