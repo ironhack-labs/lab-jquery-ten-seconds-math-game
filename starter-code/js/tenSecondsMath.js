3bb60c159a7b454384150a41769c160d41c5ff2f
Octave Lapeyronie <octave.lapeyronie@gmail.com>
2017-07-03 14:24:14 +0200

    PAR Octave+Lesly ten second math game submission

--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,13 +1,98 @@
 // Use this file to write the logic of your game, the needed attrs and functions
+var TenSecondsMathGame = function(operation, limit) {
 
-var TenSecondsMathGame = function(options) {
+  this.operation = operation;
+  this.limit = limit;
+  this.question = "";
+  this.solution = 0;
+  this.timer = 0;
+  this.qCount = 0;
 
 };
 
 // Returns a random integer between [1..numberLimit]
-
+function randomNumber(limit) {
+  return _.random(0,limit);
+}
 
 // Returns an object with {question, answer}
+TenSecondsMathGame.prototype.newQuestion = function() {
 
+  var rand1 = randomNumber(this.limit);
+  var rand2 = randomNumber(this.limit);
+
+  var operand = '';
+
+  switch (this.operation) {
+    case "addition":
+      this.solution = rand1 + rand2;
+      operand = '+';
+      break;
+    case "subtraction":
+      this.solution = rand1 - rand2;
+      operand = '-';
+      break;
+    case "multiplication":
+      this.solution = rand1 * rand2;
+      operand = '*';
+      break;
+    case "division":
+      this.solution = rand1 / rand2;
+      operand = '*';
+      break;
+  }
+
+
+  this.question = rand1 + " " + operand + " " + rand2;
+
+  this.qCount += 1;
+  console.log(this.question);
+}
 
 // Checks a user answer
+TenSecondsMathGame.prototype.isCorrectAnswer = function(answer) {
+  console.log(answer === this.solution ? "Great!" : "Wrooooong!");
+
+  if (answer === this.solution) {
+    this.newQuestion();
+    this.timer += 10000;
+    if (this.qCount == 2) { this._startTimer(); }
+  }
+
+}
+
+
+// Timer start
+TenSecondsMathGame.prototype._startTimer = function() {
+
+  var self = this;
+
+  var iterateTimer = function () {
+
+    if (self._checkTimer()) {
+      console.log(self.timer / 1000);
+      timeoutId = setTimeout(iterateTimer, 1000);
+    } else {
+      clearTimeout(timeoutId);
+      console.log('Game over!');
+    }
+  }
+
+  var timeoutId = setTimeout(iterateTimer,1000);
+}
+
+
+// Timer check
+TenSecondsMathGame.prototype._checkTimer = function() {
+  if (this.timer > 0) {
+    this.timer -= 1000;
+    return true;
+  }
+
+  return false;
+}
+
+
+// Initiate a game
+var game = new TenSecondsMathGame('addition', 10);
+game.newQuestion();
