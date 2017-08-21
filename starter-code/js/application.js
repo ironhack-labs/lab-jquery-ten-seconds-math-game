0e005d11e0fdf686e468746bc592d6455f0c0b53
Mark Allen <mark@traversify.com>
2017-08-21 17:28:35 +0200

    For will

--- starter-code/js/application.js
+++ starter-code/js/application.js
@@ -4,5 +4,44 @@
 
 
 window.onload = function(){
+	var startButton = document.getElementById("start");
+	var restartButton = document.getElementById("restart");
+	var questionArea = document.getElementById("question");
 
-};
+	startButton.addEventListener("click", function() {
+		var hideable = document.getElementById("game-options");
+		var hideable2 = document.getElementById("game-over-img");
+		var addition = document.getElementById("addition");
+		var subtraction = document.getElementById("substraction");
+		var multiplication = document.getElementById("multiplication");
+		var division = document.getElementById("division");
+		var operationsArray = [addition, subtraction, multiplication, division];
+		var filteredArray = operationsArray.filter(function(i) {
+			return i.checked === true;
+		})
+		console.log(addition)
+		if (filteredArray.length === 0) {
+			alert("Please Choose One Operator");
+			return undefined;
+		}
+		game = new TenSecondsMathGame(filteredArray, 10)
+		questionArea.innerHTML = game.newQuestion();
+		hideable.style.display = "none";
+		hideable2.style.display = "none";
+		})
+
+	restartButton.addEventListener("click", function() {
+		var answerInput = document.getElementById("answer");
+		var answer = answerInput.value
+		if (game.isCorrectAnswer(answer)) {
+			answerInput.style.border = "5px solid green";
+			answerInput.value = ''
+			questionArea.innerHTML = game.newQuestion();
+		} else {
+			answerInput.style.border = "5px solid red";
+		}
+	})
+
+
+	
+ };
--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,13 +1,50 @@
 // Use this file to write the logic of your game, the needed attrs and functions
 
-var TenSecondsMathGame = function(options) {
+var TenSecondsMathGame = function(operationsArray, numberLimit) {
+	this.operationArray = operationsArray
+	if (numberLimit > 10) {
+		this.upperNumberLimit = numberLimit 
+	} else {
+		this.upperNumberLimit = 10
+	};
 
 };
 
-// Returns a random integer between [1..numberLimit]
+TenSecondsMathGame.prototype.newQuestion = function() {
+	this.firstNumber = Math.floor(Math.random() * this.upperNumberLimit + 1);
+	this.secondNumber = Math.floor(Math.random() * this.upperNumberLimit + 1);
+	this.tempNumber = 0
+	if (this.secondNumber > this.firstNumber) {
+		this.tempNumber = this.firstNumber;
+		this.firstNumber = this.secondNumber;
+		this.secondNumber = this.tempNumber;
+	}
+	this.operation = this.operationArray[Math.floor(Math.random() * this.operationArray.length)]
+	if (this.operation.includes("addition")) { 
+		this.calculation = this.firstNumber + this.secondNumber;
+	} else if (this.operation.includes("substraction")) {
+		this.calculation = this.firstNumber - this.secondNumber;
+	} else if (this.operation.includes("multiplication")) {
+		this.calculation = this.firstNumber * this.secondNumber;
+	} else {
+		this.calculation = this.firstNumber / this.secondNumber;
+	}
+	this.questionAsked = this.firstNumber + " " + this.operation + " " + this.secondNumber
+	return (this.questionAsked)
+	// return {"question": this.questionAsked, "answer": this.calculation};
+}
 
 
-// Returns an object with {question, answer}
+TenSecondsMathGame.prototype.isCorrectAnswer = function(userAnswer) {
+	if (userAnswer == this.calculation) { 
+		return true
+} else {
+		return false
+}
+}
 
 
-// Checks a user answer
+var game = new TenSecondsMathGame(["+", "-", "x", "/"], 7)
+// debug(game.newQuestion()["answer"])
+
+
