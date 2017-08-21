2d3134212f089e29e056b32e729ce90f8331db04
ritorrehuelamo <torrehuelamo@gmail.com>
2017-08-21 16:42:39 +0200

    Iteration 1 - Question Generator Done

--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,26 +1,39 @@
-
-function Game (option, numberLimit) {
-    this.option = option
-    this.numberLimit = numberLimit
+function Game(option, numberLimit) {
+	this.option = option,
+	this.numberLimit = numberLimit,
+	this.firstLimit = 0
 }
 
 Game.prototype.newQuestion = function () {
-    this.numberX = parseInt(Math.random() * 10 + 1)
-    this.numberY = parseInt(Math.random() * 10 + 1)
-    this.result = this.numberX + this.numberY
-    console.log(this.numberX + '+' + this.numberY)
+	var a = this.getRandomNumber()
+	var b = this.getRandomNumber()
+	while(Math.abs(a - b) <= 3) {
+		b = this.getRandomNumber()
+	}
+	console.log('Suma de ' + a + '+' + b)
+	this.result = a + b
+}
+
+Game.prototype.getRandomNumber = function () {
+	return parseInt(Math.random() * 10 + 1)
 }
 
 Game.prototype.isCorrectAnswer = function (a) {
-    var resp =  (this.result == a) ? 'Correct' : 'Incorrect'
-    return resp
+	if (a > 0) {
+		return (this.result == a) ? 'Correct' : 'Incorrect'
+	} else {
+		return 'The number must be grater than 0'
+	}
 }
 
-var game = new Game('addition', 10)
+window.onload = function () {
+	var game = new Game('addition', 100)
+	game.newQuestion()
+}
 
 // Use this file to write the logic of your game, the needed attrs and functions
 
-var TenSecondsMathGame = function(options) {
+var TenSecondsMathGame = function (options) {
 
 };
 
