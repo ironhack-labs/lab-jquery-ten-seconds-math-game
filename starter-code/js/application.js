73632ec41d4b75b51ab23387a941d42893685435
mir592 <miriam5592@gmail.com>
2017-08-21 19:20:26 +0200

    guardar2

--- starter-code/js/application.js
+++ starter-code/js/application.js
@@ -1,10 +1,3 @@
 // Use this file to write the interactions between your game and the user
 
 //Initialize ion library
-
-
-window.onload = function(){
-var game = new TenSecondsMathGame("suma", 100);
-game.operations();
-game.answer();
-};
--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -3,45 +3,59 @@
 var TenSecondsMathGame = function(options, limit){
   this.options = options;
   this.limit = limit;
-  this.n1 = 0;
-  this.n2 = 0;
   this.result = 0;
   this.operacion = 0;
-  this.ans = 0;
+  this.time = 10;
   };
 // Returns a random integer between [1..numberLimit]
-TenSecondsMathGame.prototype.randomNum = function(max){
-  return Math.floor(Math.random()*(max - 1) + 1);
+TenSecondsMathGame.prototype.randomNum = function(){
+  return Math.floor(Math.random()*(this.limit - 1) + 1);
 };
+
+
 
 TenSecondsMathGame.prototype.operations = function(){
-  this.n1 = this.randomNum(this.limit);
-  console.log(this.n1);
-  this.n2 = this.randomNum(this.limit);
-  console.log(this.n2);
-  while (this.n1 - this.n2 < 3){
-    this.n2 = this.randomNum(this.limit);
+  var n1 = this.randomNum();
+  var n2 = this.randomNum();
+
+
+  while (Math.abs(n1 - n2) < 3){
+    n2 = this.randomNum();
   }
+
   switch (this.options){
     case 'suma':
-      this.result = this.n1 + this.n2;
-      this.operacion = this.n1 + " + " + this.n2;
+      this.result = n1 + n2;
+      this.operacion = n1 + " + " + n2;
     break;
     }
-  console.log(this.operacion);
+    console.log(this.operacion);
+    return this.result;
 };
-TenSecondsMathGame.prototype.answer= function(answ){
-  console.log(this.operacion);
-  this.ans = prompt(this.operacion, "ONLY NUMBERS");
-  if(this.ans != this.result){
-    console.log("WRONG!");
-  }else{
+
+TenSecondsMathGame.prototype.answer = function(ans){
+  if(ans === this.result){
     console.log("GREAT!");
+  }else{
+    console.log("WRONG!");
   }
 };
 
+TenSecondsMathGame.prototype.time = function () {
+  var that = this;
+  var intervalId = setInterval(function() {
+    if (that.time > 0) {
+      console.log(i);
+    } else {
+      console.log("Pop!");
+      clearInterval(intervalId);
+    }
 
+    that.time--;
+  }, 1000);
+};
 
+var game = new TenSecondsMathGame("suma", 10);
 // Returns an object with {question, answer}
 
 
