bfbb796865cbf5a328cfaf3607fe331d7aa829b8
ritorrehuelamo <torrehuelamo@gmail.com>
2017-08-21 13:02:57 +0200

    Iteration 1 - newQuestion, isCorrectAnswer

--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,3 +1,23 @@
+
+function Game (option, numberLimit) {
+    this.option = option
+    this.numberLimit = numberLimit
+}
+
+Game.prototype.newQuestion = function () {
+    this.numberX = parseInt(Math.random() * 10 + 1)
+    this.numberY = parseInt(Math.random() * 10 + 1)
+    this.result = this.numberX + this.numberY
+    console.log(this.numberX + '+' + this.numberY)
+}
+
+Game.prototype.isCorrectAnswer = function (a) {
+    var resp =  (this.result == a) ? 'Correct' : 'Incorrect'
+    return resp
+}
+
+var game = new Game('addition', 10)
+
 // Use this file to write the logic of your game, the needed attrs and functions
 
 var TenSecondsMathGame = function(options) {
