5ac2847276d33f9661792d43a2133485a91add33
Álvar <alvar@MacBook-Air-de-Alvar.local>
2017-08-21 17:53:33 +0200

    a bit

--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,8 +1,46 @@
-// Use this file to write the logic of your game, the needed attrs and functions
+function TenSecondsMathGame(ops, numberLimit) {
+  this.ops = ops;
+  this.numberLimit = numberLimit;
+  this.underLimit = 0;
+  this.result = 0;
+}
 
-var TenSecondsMathGame = function(options) {
-
+TenSecondsMathGame.prototype.getRandomNumber = function(){
+  return parseInt(Math.random()*this.numberLimit +1);
 };
+
+TenSecondsMathGame.prototype.question = function(){
+  var num1 = this.getRandomNumber();
+  var num2 = this.getRandomNumber();
+
+
+  if((Math.abs(num1-num2))<=3){
+    return this.question();
+  }
+  else{
+    console.log('Voy a devolverte los siguientes números : '+ num1 +' y '+num2 );
+    this.result = num1 + num2;
+  }
+  return this.result;
+};
+
+
+TenSecondsMathGame.prototype.answer= function(answer){
+
+  if(answer === this.result) {
+    console.log("Correcto!!!");
+    this.question();
+
+  }else{
+    console.log("No es correcto");
+  }
+};
+
+
+var game = new TenSecondsMathGame('substract', 10);
+var game2 = new TenSecondsMathGame('substract', 100);
+
+
 
 // Returns a random integer between [1..numberLimit]
 
