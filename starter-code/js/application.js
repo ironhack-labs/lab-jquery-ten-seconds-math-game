0a56632aee076435c2512adb059f398f6c24268e
y4izus <yaiza.garcia.mm@gmail.com>
2017-08-21 18:13:15 +0200

    Iteration 1.1.-Building a question generator DONE

--- starter-code/js/application.js
+++ starter-code/js/application.js
@@ -1,8 +1,4 @@
 // Use this file to write the interactions between your game and the user
 
 //Initialize ion library
-
-
-window.onload = function(){
-
-};
+var game = new TenSecondsMathGame(['addition', 'multiplication'], 10)
