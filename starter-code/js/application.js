0086ca9983642b3b8b9ffef8d2bd389fd9ae3d1f
VeroGlez <veronicagonzalezperez@MacBook-Pro-de-Veronica.local>
2017-08-21 18:18:05 +0200

    Sum operation

--- starter-code/js/application.js
+++ starter-code/js/application.js
@@ -4,5 +4,37 @@
 
 
 window.onload = function(){
+  var time = 10;
+  var operation = "add";
+  //TenSecondsMathGame();
+  var game = new TenSecondsMathGame(operation, time);
+
+
+  var startGame = document.getElementById('start');
+
+  startGame.onclick = function(){
+    game.control = true;
+    game.question();
+
+    var enter = document.getElementById('restart');
+    enter.onclick = function(){
+      b = document.getElementById('answer').value;
+      game.compareResult(game.sum, b);
+
+    };
+
+
+    // Counter
+    var timeoutId = setInterval(function(){
+      if(game.count > 0){
+        game.timer();
+        console.log(game.count);
+      }else{
+        clearInterval(timeoutId);
+        game.control = false;
+      }
+    }, 1000);
+
+  };
 
 };
--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,13 +1,67 @@
 // Use this file to write the logic of your game, the needed attrs and functions
 
-var TenSecondsMathGame = function(options) {
-
+var TenSecondsMathGame = function(ops) {
+  this.ops = ops;
+  this.numberLimit = 10;
+  this.underLimit = 0;
+  this.count =10;
+  this.sum =0;
+  this.control = false;
 };
 
-// Returns a random integer between [1..numberLimit]
+//Generate random number
+TenSecondsMathGame.prototype.generateRandomNumber = function(){
+  var max = 10;
+  return parseInt(Math.random()*10)+1;
+};
 
+TenSecondsMathGame.prototype.timer = function(){
+  this.count -=1;
+  //console.log(this.count);
+};
+//Generate the correct answer
+TenSecondsMathGame.prototype.question = function(){
+  if (this.control){
+    var a = this.generateRandomNumber();
+    var b = this.generateRandomNumber();
 
-// Returns an object with {question, answer}
+    while ( Math.abs(a-b) < 3 ){
+      a = this.generateRandomNumber();
+    }
 
+    this.sum = this.sumNumbers(a, b);
+    console.log(a,b);
+    console.log("suma: ", this.sum);
+  }
+};
 
-// Checks a user answer
+// Sum two values
+TenSecondsMathGame.prototype.sumNumbers = function(a, b){
+  this.sum = a+b;
+  return a+b;
+};
+
+//Compare result
+TenSecondsMathGame.prototype.compareResult = function(a, b){
+  if (a == b){
+    if (this.control){
+      console.log("You are right!");
+      this.question();
+    }else{
+      return;
+    }
+  }else{
+    if (this.control){
+      console.log("You are wrong!");
+    }else{
+      return;
+    }
+  }
+};
+
+TenSecondsMathGame.prototype.questionInfo = function(){
+  //a = getElementById('answer');
+
+  a = prompt("Insert number: ");
+  return a;
+};
