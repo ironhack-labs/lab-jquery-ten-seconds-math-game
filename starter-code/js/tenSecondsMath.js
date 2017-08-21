e864e029b3d6c4a4e1e18bb2d3a8e65531620c30
Luisa Fernanda Salazar Hoyos <luisafernandasalazarhoyos@MacBook-Pro-de-Luisa.local>
2017-08-21 13:40:55 +0200

    created random

--- starter-code/js/tenSecondsMath.js
+++ starter-code/js/tenSecondsMath.js
@@ -1,9 +1,35 @@
 // Use this file to write the logic of your game, the needed attrs and functions
 
-var TenSecondsMathGame = function(options) {
+var TenSecondsMathGame = function( max) {
+  this.operation = "add";
+  this.max = max;
 
 };
 
+TenSecondsMathGame.prototype.randomNumber = function () {
+  return Math.floor( Math.random()*(this.max-1)+1);
+};
+
+TenSecondsMathGame.prototype.giveQuestion = function (){
+  this.firstNumber = this.randomNumber();
+  this.secondNumber = this.randomNumber();
+  this.result = this.firstNumber + this.secondNumber;
+  console.log(this.firstNumber+ "+" + this.secondNumber);
+};
+
+TenSecondsMathGame.prototype.checkNumber = function (number){
+  if(number == this.result){
+    return this.giveQuestion();
+  }else{
+    console.log("Intenatalo otra vez!!");
+  }
+};
+
+
+// function generateRandom () {
+//   var firstNumber = TenSecondsMathGame.randomNumber;
+//   console.log(firstNumber);
+// }
 // Returns a random integer between [1..numberLimit]
 
 
